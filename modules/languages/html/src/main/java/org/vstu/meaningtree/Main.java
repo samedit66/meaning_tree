cf1b21fbf2109339492d0a600208784e2e806bbb
bardoor <dmitrybartenev58@gmail.com>
2025-03-31 00:52:38 +0300

    feat(html): добавил примерчик шаблончика freemaker

--- modules/languages/html/src/main/java/org/vstu/meaningtree/Main.java
+++ modules/languages/html/src/main/java/org/vstu/meaningtree/Main.java
@@ -1,7 +1,39 @@
 package org.vstu.meaningtree;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, TemplateException {
+        ClassLoader classLoader = Main.class.getClassLoader();
+
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_34);
+        File templateDir = new File(
+                Objects.requireNonNull(classLoader.getResource("templates")).getPath()
+        );
+
+        cfg.setDirectoryForTemplateLoading(templateDir);
+        cfg.setLogTemplateExceptions(true);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(true);
+
+        /* Create data-model */
+        Map<String, String> root = new HashMap<>();
+        root.put("name", "John");
+
+        Template template = cfg.getTemplate("hello.ftlh");
+
+        Writer out = new OutputStreamWriter(System.out);
+        template.process(root, out);
+
     }
 }
